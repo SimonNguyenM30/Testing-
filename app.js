b1e9f73d13858f99ce273934df91499729ea9647
Simon nguyen <simonnguyen@MacBook-Pro-3.local>
2016-10-17 10:51:32 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import SearchBar from '/components/searchBar';
+
+const API_KEY = 'AIzaSyD9JHXhgO_JHKUQ1h_CeECfEDbNG8DxR0k';
+
+class App extends React.Component {
+	render(){
+		return (
+			<div>
+				<SearchBar />
+			</div>
+		);
+	}
+}
+
+export default App   
